13f60b25811cb8b8323d289d94e216d928474fca
claudmiine <wojcieska.klaudia@gmail.com>
2020-06-28 19:01:23 +0200

    ex. 21

--- ex21.js
+++ ex21.js
@@ -1,1 +1,8 @@
-//I need to do more exercises. 
+// Write a JavaScript program to concatenate two strings except their first character. 
+
+function concatenate(str1, str2) {
+str1 = str1.substring(1, str1.length);
+str2 = str2.substring(1, str2.length);
+return str1 + " " + str2;
+}
+console.log(concatenate("Hello", "hejo"))
